fe57f114fe8bab2b42a81887e8d6c3ac706ad5de
KaustubhPatange <developerkp16@gmail.com>
2024-08-21 22:37:27 +0530

    add run script

---
+++ hugo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! command -v "hugo" &> /dev/null
+then
+    echo "Error: hugo is not installed or not found in PATH. Please check installation script"
+    exit 1
+fi
+
+echo "Transpiling..."
+hugo
